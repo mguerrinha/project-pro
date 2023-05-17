d00b68f094052f00da2007eaa2495cfbd7a4491c
tommyvercetti10 <132352943+tommyvercetti10@users.noreply.github.com>
2023-05-17 14:10:33 +0100

    prontxinho

--- XPM2.hpp
+++ XPM2.hpp
@@ -8,6 +8,8 @@ namespace prog {
     int hex_to_decimal(const std::string& str);
     Color hex_to_color(const std::string& str);
     Image* loadFromXPM2(const std::string &file);
+    std::string int_to_hex(int value);
+    std::string color_to_hex(const Color& color);
     void saveToXPM2(const std::string& file, const Image* img);
 }
 #endif
